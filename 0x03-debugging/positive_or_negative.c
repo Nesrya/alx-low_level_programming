42dd5e43b5f89b72f564896ec036319de3a3e808
Nesrya <nesryay@gmail.com>
2023-06-21 00:56:25 -0700

    positive_negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - shows if a number is positive or negative
+(* 0 : is the number to be chacked
+ *Return: 0 success
+ */
+
+int positive_or_negative(int i)
+{
+if (i < 0)
+{
+printf("%d is %s\n", i, "negative");
+}
+else if (i < 0)
+{
+printf("%d is %s\n", i, "positive");
+}
+else
+{
+printf("%d is %s\n", i, "zero");
+}
+return;
+
+}
