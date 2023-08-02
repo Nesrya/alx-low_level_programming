aaa9f4ed1cb463c95e057ca2800d408de2769582
Nesrya <nesryay@gmail.com>
2023-08-01 17:08:28 -0700

    n 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - this returns the node at a certain index in a linked list
+ * get_nodeint_at_index -  returns the node at certain index in a linked list
  * @head: the first node in the linked list
  * @index: an index of the node to return
  *
